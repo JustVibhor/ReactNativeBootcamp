002ffd788a5820efaf81f615182578b13bdbed8e
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-04 12:39:52 +0530

    my first app, kind off

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>working properly!!</Text>
+      <Text>My first app, Kind off</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFC0CB',
     alignItems: 'center',
     justifyContent: 'center',
   },
