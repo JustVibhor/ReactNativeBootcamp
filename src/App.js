dc5ae61768872de9efef7a7e1623408fac24a0ee
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-05 22:08:03 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import React from 'react';
 import './App.css';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 
 function App() {
+
+  const [courseGoals, setCourseGoals] = useState([
+    {id: "cg1", text: "Finish the course"},
+    {id: "cg2", text: "Learn everything about the course"},
+    {id: "cg3", text: "Help others with your knowledge"}
+  ]);
+
+
+  const addNewGoalHandler = newGoal => {
+    setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
+  }
+
   return (
-    <div className="App">
+    <div className="course-goals">
       <h2>Course Goals</h2>
-      <ul>
-        <li>follow</li>
-        <li>follow</li>
-        <li>follow</li>
-        <li>follow</li>
-        <li>follow</li>
-      </ul>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals}/>
     </div>
   );
 }
