9b6bd473dad22ea88baa1c234e98776049641337
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-05 12:03:10 +0530

    react refresher

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Course Goals</h2>
+      <ul>
+        <li>follow</li>
+        <li>follow</li>
+        <li>follow</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
